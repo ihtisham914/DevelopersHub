0a917bf052ff5edb06217033d09facd2dfdb5e4b
Ihtisham Ul haq <you@example.com>
2022-10-11 07:43:57 +0500

    code refined

--- src/App.js
+++ src/App.js
@@ -6,35 +6,6 @@ import Developers from "./components/Developers";
 
 function App() {
   AOS.init();
-  const person = [
-    {
-      id: 1,
-      name: "Ihtisham ul haq",
-      skill: "Nuxtjs",
-      desc: "Nuxtjs developer Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, libero labore dolore accusamus excepturi neque ",
-    },
-    {
-      id: 2,
-      name: "Umair ahmad khan",
-      skill: "Flutter",
-      desc: "Flutter developer Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, libero labore dolore accusamus excepturi neque ",
-    },
-    {
-      id: 3,
-      name: "Hikmat Bangash",
-      skill: "Full Stack",
-      desc: "Full stack developer Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, libero labore dolore accusamus excepturi neque ",
-    },
-    {
-      id: 4,
-      name: "Masab Gull",
-      skill: "Andriod",
-      desc: "Andriod developer Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, libero labore dolore accusamus excepturi neque ",
-    },
-  ];
-
-  const [data, setData] = useState(person);
-
   return (
     <div className="flex items-center justify-center pt-2rem pl-5rem pr-5rem  w-80rem">
       <Developers />
